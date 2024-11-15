88e623da08f4dfd4e99485712ca7326c7fdd511c
dd <kyjoon0105@gmail.com>
2024-11-15 18:26:02 +0900

    11/15

--- w1115/w03Project/students/views.py
+++ w1115/w03Project/students/views.py
@@ -9,6 +9,7 @@ def write(request):
 # 학생 리스트 가져오기
 def list(request):
 	qs = Student.objects.all()
+	# {'list':qs} 에서 'list' 는 qs 담는 변수 
 	context = {'list':qs}
 	return render(request,'stu_list.html',context)
 
